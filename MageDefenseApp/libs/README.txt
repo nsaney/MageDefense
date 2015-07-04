b10a9b269f9354b50b2429e47b9cddd120b88c00
nsaney <nsaney@gmail.com>
2015-07-03 20:50:18 -0400

    Added MageDefenseApp for Android app
    Finally adding to source control.

---
+++ MageDefenseApp/libs/README.txt
@@ -0,0 +1,5 @@
+Step 1 of 2:
+Build the QuadradoAndroid project and copy the contents of the Deploy_QuadradoAndroid directory next to this file (into this project's /libs folder).
+
+Step 2 of 2:
+Build the MageDefense project and copy the Run_MageDefense.jar file next to this file.
---
+++ MageDefenseApp/src/chairosoft/mage_defense/MageDefenseLauncherActivity.java
@@ -0,0 +1,14 @@
+package chairosoft.mage_defense;
+
+import chairosoft.android.QuadradoLauncherActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MageDefenseLauncherActivity extends QuadradoLauncherActivity
+{
+    public MageDefenseLauncherActivity()
+    {
+        super(chairosoft.mage_defense.MageDefense.class);
+    }
+}
